9f843ad079d48edfbf98322012d372a8f572f21c
vimal <vimalmahanthy@gmail.com>
2022-11-06 01:13:05 +0530

    pointer study

--- main.c
+++ main.c
@@ -29,27 +29,36 @@ int main()
 
     printf("Hello world!stupid\n");
 
-    for(unint8 lop_var=0;lop_var<MAX_STUDENTS;lop_var++,ptr2studnt++)
+    for(unint8 lop_var=0;lop_var<MAX_STUDENTS;lop_var++)
     {
-        ptr2studnt->name =   "hi";
-        (ptr2studnt->age)   =   lop_var;
-    (ptr2studnt->height)   =   6+lop_var;
-    (ptr2studnt->exammarks.eng)    =   50+lop_var;
-    (ptr2studnt->exammarks.hin)    =   60+lop_var;
-    ptr2studnt->exammarks.tel   =   70+lop_var;
+        ptr2studnt[lop_var].name            =   "hi";
+        ptr2studnt[lop_var].age             =   lop_var;
+        ptr2studnt[lop_var].height          =   6+lop_var;
+        ptr2studnt[lop_var].exammarks.eng   =   50+lop_var;
+        ptr2studnt[lop_var].exammarks.hin   =   60+lop_var;
+        ptr2studnt[lop_var].exammarks.tel   =   70+lop_var;
 
+        ptr2studnt[lop_var].ptr2marks       =   &ptr2studnt[lop_var].exammarks;
+
+        printf("addr of ptr2sstudnt[%d].exmmarks is %x\t" ,lop_var,&(ptr2studnt[lop_var].exammarks));
+        printf("addr of ptr2studnt[%d].ptr2marks is %x\t\n",lop_var,ptr2studnt[lop_var].ptr2marks);
     }
 
-    ptr2studnt  =   &students[0];
+    //ptr2studnt[0].ptr2marks  =   &students[0].exammarks;
+    //ptr2studnt[1].ptr2marks  =   &students[1].exammarks;
 
     for(unint8 lop_var=0;lop_var<MAX_STUDENTS;lop_var++)
     {
-        //printf("u is %d\n",students[0]->ptr2marks[lop_var].eng); //run time fault
+        printf("\addr of ptr2studnt->ptr2marks[lopvar].eng is %x\t",
+               (ptr2studnt[lop_var].ptr2marks)); //run time fault
+
+        printf("\value at ptr2studnt->ptr2marks[lopvar].eng is %d\n",
+               (ptr2studnt[lop_var].ptr2marks->eng)); //run time fault
         //printf("u is %d\n",students[0].ptr2marks[lop_var]->eng); compile time error
         //printf("u is %d\n",students[lop_var].ptr2marks->eng);run time error
         //printf("u is %d\n",students[lop_var].ptr2marks->eng);
         //printf("name is %d\n",students->ptr2marks[lop_var].eng);
-        printf("name is %d\n",ptr2studnt->exammarks.eng);
+       // printf("name is %d\n",ptr2studnt->exammarks.eng);
         //printf("name is %d\n",(students[lop_var]->exammarks.eng));
     }
 
